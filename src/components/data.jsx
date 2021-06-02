737fa224d41f934c579221d3615dd7a106ecf5c2
sibnajib <nasitnajis@gmail.com>
2021-06-02 13:23:06 -0700

    JPG transform

--- src/components/data.jsx
+++ src/components/data.jsx
@@ -1,5 +1,5 @@
 export const data = [
-  { id: 1, name: "Abir", age: 19, bio: "../../pics/a.jpg" },
+  { id: 1, name: "Abir", age: 19, bio: "../../pics/a.JPG" },
   { id: 2, name: "Asif", age: 15, bio: "../../pics/b.jpg" },
   { id: 3, name: "Amit", age: 16, bio: "../../pics/c.jpg" },
   { id: 4, name: "Azim", age: 13, bio: "../../pics/d.jpg" },
